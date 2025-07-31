13d777b0e54bb26197e57ecb09bc402f286b114d
XoDeR <xoder.vs@gmail.com>
2025-07-31 18:52:09 +0200

    basic article and block components

---
+++ resources/js/components/Article/Article.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import Block from '../Block/Block.vue';
+
+</script>
+<template>
+  <Block />
+</template>
---
+++ resources/js/components/Block/Block.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+<template>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, non esse dolorem itaque ipsa deleniti nostrum
+    impedit obcaecati minima quae at dolores qui ad dolorum deserunt omnis quisquam officiis maiores.</p>
+</template>
